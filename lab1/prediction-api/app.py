f078d67cd2c5e6a6c7a6fa3c2f4f183ceea41491
IndikaKuma <ipkumarawd@yahoo.com>
2023-01-30 16:54:40 +0100

    Remove PV add GCP bucket

--- lab1/prediction-api/app.py
+++ lab1/prediction-api/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, request
 
 from diabetes_predictor import DiabetesPredictor
@@ -6,7 +8,12 @@ app = Flask(__name__)
 app.config["DEBUG"] = True
 
 
-@app.route('/diabetes_predictor/', methods=['POST']) # path of the endpoint. Except only HTTP POST request
+@app.route('/diabetes_predictor/model', methods=['PUT'])  # trigger updating the model
+def refresh_model():
+    return dp.download_model()
+
+
+@app.route('/diabetes_predictor', methods=['POST'])  # path of the endpoint. Except only HTTP POST request
 def predict_str():
     # the prediction input data in the message body as a JSON payload
     prediction_inout = request.get_json()
@@ -14,4 +21,4 @@ def predict_str():
 
 
 dp = DiabetesPredictor()
-app.run(host='0.0.0.0', port=5000)
+app.run(port=int(os.environ.get("PORT", 5000)), host='0.0.0.0', debug=True)
--- lab1/training-api/resources/model_trainer.py
+++ lab1/training-api/resources/model_trainer.py
@@ -6,9 +6,13 @@ import os
 from flask import jsonify
 from keras.layers import Dense
 from keras.models import Sequential
+from google.cloud import storage
 
 
 def train(dataset):
+    project_id = os.environ.get('PROJECT_ID', 'Specified environment variable is not set.')
+    model_repo = os.environ.get('MODEL_REPO', 'Specified environment variable is not set.')
+    model_name = os.environ.get('MODEL_NAME', 'Specified environment variable is not set.')
     # split into input (X) and output (Y) variables
     X = dataset[:, 0:8]
     Y = dataset[:, 8]
@@ -28,14 +32,16 @@ def train(dataset):
         "loss": scores[0],
     }
     logging.info(text_out)
-    print(text_out)
+
+    model_path = os.path.join('/tmp', model_name)
+    model.save(model_path)
+    # Save to GCS
+    client = storage.Client(project=project_id)
+    bucket = client.get_bucket(model_repo)
+    blob = bucket.blob(model_name)
+    blob.upload_from_filename(model_path)
+    # Do clean up
+    os.remove(model_path)
+
     # Saving model in a given location provided as an env. variable
-    model_repo = os.environ['MODEL_REPO']
-    if model_repo:
-        file_path = os.path.join(model_repo, "model.h5")
-        model.save(file_path)
-        logging.info("Saved the model to the location : " + model_repo)
-        return jsonify(text_out), 200
-    else:
-        model.save("model.h5")
-        return jsonify({'message': 'The model was saved locally.'}), 200
+    return jsonify(text_out), 200
