c8545eb5102fa625b48075d0df8f509d19690c60
IndikaKumara <ipkumarawd@yahoo.com>
2023-04-13 14:42:38 +0200

    Update caching for inventoryservice

--- lab5/inventoryservice/app.py
+++ lab5/inventoryservice/app.py
@@ -1,31 +1,16 @@
 from flask import Flask, request
 
 from resources.product import Product, Products
-from datetime import datetime, date, timedelta
-from functools import wraps
-from flask import Response
 
 app = Flask(__name__)
 product = Product()
 products = Products()
 
 
-def docache(minutes=5, content_type='application/json; charset=utf-8'):
-    """ Flask decorator that allow to set Expire and Cache headers. """
-
-    def fwrap(f):
-        @wraps(f)
-        def wrapped_f(*args, **kwargs):
-            r = f(*args, **kwargs)
-            then = datetime.now() + timedelta(minutes=minutes)
-            rsp = Response(r, content_type=content_type)
-            rsp.headers.add('Expires', then.strftime("%a, %d %b %Y %H:%M:%S GMT"))
-            rsp.headers.add('Cache-Control', 'public,max-age=%d' % int(60 * minutes))
-            return rsp
-
-        return wrapped_f
-
-    return fwrap
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 300
+    return response
 
 
 @app.route('/products/', methods=['POST'])
@@ -38,7 +23,6 @@ def create_products_from_query():
     return products.post_query(request)
 
 
-@docache(minutes=4, content_type='application/json')
 @app.route('/products/<string:pname>', methods=['GET'])
 def get_order(pname):
     return product.get(pname)
