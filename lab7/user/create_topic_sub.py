f16a2d53e02c26148c4421850990e3cb6b9747a9
IndikaKuma <ipkumarawd@yahoo.com>
2023-04-13 20:02:57 +0200

    Enable Logging for Create Topic

--- lab7/user/create_topic_sub.py
+++ lab7/user/create_topic_sub.py
@@ -1,6 +1,9 @@
+import logging
+
 from user_consumer import create_subscription
 from user_publisher import create_topic
 
+logging.getLogger().setLevel(logging.INFO)
 create_topic("your_project_id", "order_req") # make sure to change the project id
 create_subscription("your_project_id", "order_req", "order_req_sub")
 create_topic("your_project_id", "inventory_status")
