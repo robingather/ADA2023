f4593217874617c0b63cc3a334c8f0b3a7bdcc75
IndikaKuma <ipkumarawd@yahoo.com>
2023-04-19 21:37:45 +0200

    Update Airflow Example

--- lab8/airflow-example1/workflow.py
+++ lab8/airflow-example1/workflow.py
@@ -1,6 +1,8 @@
 import datetime
 
 from airflow import models
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateEmptyDatasetOperator
+from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.providers.google.cloud.transfers.gcs_to_local import GCSToLocalFilesystemOperator
 from airflow.providers.google.cloud.transfers.local_to_gcs import LocalFilesystemToGCSOperator
 
@@ -12,7 +14,7 @@ from custom_ops import CustomTransformationOperator
 YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
 default_args = {
-    'owner': 'Composer Example',
+    'owner': 'ADA',
     'depends_on_past': False,
     'email': [''],
     'email_on_failure': False,
@@ -23,27 +25,43 @@ default_args = {
 }
 
 with models.DAG(
-        'composer_quickstart',
+        'simple_etl',
         catchup=False,
         default_args=default_args,
         schedule_interval=datetime.timedelta(days=1)) as dag:
-
-    download = GCSToLocalFilesystemOperator(
-        task_id="download_data",
+    download_from_gcs = GCSToLocalFilesystemOperator(
+        task_id="download_from_gcs",
         object_name="sales.csv",
         bucket="adadata",
-        filename="/tmp/sales.csv"
+        filename="/home/airflow/gcs/data/sales.csv"
     )
 
     transform = CustomTransformationOperator(
-        task_id='transform_data', file_name='/tmp/sales.csv')
+        task_id='transform_data', file_name='/home/airflow/gcs/data/sales.csv')
 
-    upload = LocalFilesystemToGCSOperator(
-        task_id='upload_data',
+    upload_to_gsc = LocalFilesystemToGCSOperator(
+        task_id='upload_to_gsc',
         bucket='adadata',
         src="{{ task_instance.xcom_pull(task_ids='transform_data') }}",
         # See https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/xcoms.html for Xcoms
         dst='sales_sum.csv'
     )
 
-    download >> transform >> upload  # https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/overview.html#control-flow
+    create_bq_dataset = BigQueryCreateEmptyDatasetOperator(
+        task_id='create_bq_dataset', dataset_id="sales"
+    )
+
+    load_to_table = GCSToBigQueryOperator(
+        task_id='load_to_bq_table',
+        bucket='adadata',
+        source_objects=['sales_sum.csv'],
+        destination_project_dataset_table="sales.summary",
+        schema_fields=[
+            {'name': 'customer_id', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+            {'name': 'unit_sales', 'type': 'INTEGER', 'mode': 'NULLABLE'}
+        ],
+        write_disposition='WRITE_TRUNCATE'
+    )
+
+    # About control flow, https://airflow.apache.org/docs/apache-airflow/stable/core-concepts/overview.html#control-flow
+    download_from_gcs >> transform >> upload_to_gsc >> create_bq_dataset >> load_to_table
